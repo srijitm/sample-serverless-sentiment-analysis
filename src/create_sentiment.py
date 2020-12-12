6d54eb97f39c287c063afc8bd083a3e5f1c1f6c0
Guillermo Mansilla <mansillag@gmail.com>
2020-12-12 18:43:47 -0500

    Function to receive data from API GW and push to SQS

--- src/create_sentiment.py
+++ src/create_sentiment.py
@@ -1,4 +1,24 @@
 import boto3
+import os
+import json
+
 
 def lambda_handler(event, context):
-    return {'result': 1}
+    """Main Handler: gets event data and pushes it to SQS"""
+    try:
+        sqs_client = boto3.client('sqs')
+        queue_name = os.environ['queueName']
+        body = json.loads(json.dumps(event['body']))
+        result = sqs_client.send_message(QueueUrl=queue_name, MessageBody=body)
+        if result.get('Failed'):
+            raise Exception("Message could not be sent to SQS")
+
+        return {
+            "statusCode": 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({"Success": True})
+        }
+
+    except Exception as err:
+        print("Error: ", err)
+        raise err
